b12176944675ed9fcff4e6f36d331fc5b3ce42d7
Prashant Sharma <randomartist10@gmail.com>
2025-09-16 00:05:05 +0545

    Added single letter animations combined with words animations

--- Assets/TranscriptionWebGL/Scripts/VoiceCommandHandlerDemo.cs
+++ Assets/TranscriptionWebGL/Scripts/VoiceCommandHandlerDemo.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,9 +23,23 @@ namespace Devden.STT
 
         [SerializeField] private Toggle listenContinuously;
 
+        private Dictionary<string, string> wordAnimations = new Dictionary<string, string>();
+        private string DefinedWords = "Hello and welcometo AbuDhabi Customs We are here to assist and support our community Our authority is to ban and block bad business Our job is to assess all goods and ban anything illegal We support a secure future for this area";
+        private Dictionary<string, float> AnimationClipLength = new Dictionary<string, float>();
+        private Coroutine animationCoroutine;
         private void Start()
         {
             TranscriptionHandler.SetGameObjectName(transform.gameObject.name);
+
+            InitializeWordAnimations();
+
+            foreach (var clip in characterAnimator.runtimeAnimatorController.animationClips)
+            {
+                if (!AnimationClipLength.ContainsKey(clip.name))
+                {
+                    AnimationClipLength.Add(clip.name.ToLower(), clip.length);
+                }
+            }
         }
         public void StartListening()
         {
@@ -33,6 +49,7 @@ namespace Devden.STT
         public void StopListening()
         {
             TranscriptionHandler.StopRecognition();
+          
         }
 
         //Receives result from speech recognition, use the same function name in case it needs to be used elsewhere.
@@ -40,6 +57,20 @@ namespace Devden.STT
         {
             outputMessageText.text = text;
             CheckPhraseMatch(text);
+        }
+        private void InitializeWordAnimations()
+        {
+
+            string cleanedDefinedWords = CleanText(DefinedWords);
+            string[] definedWordsArray = cleanedDefinedWords.Split(' ');
+
+            foreach (string word in definedWordsArray)
+            {
+                if (!string.IsNullOrEmpty(word) && !wordAnimations.ContainsKey(word))
+                {
+                    wordAnimations.Add(word, $"{word}");
+                }
+            }
         }
 
         private string CleanText(string textinput)
@@ -76,6 +107,55 @@ namespace Devden.STT
             }
 
             Debug.Log($"No matching phrase found for: {recognizedText}");
+            ProcessWordsIndividually(cleanedRecognizedText);
+
+        }
+
+        private void ProcessWordsIndividually(string text)
+        {
+            string[] words = text.Split(' ');
+
+            animationCoroutine = StartCoroutine(ProcessWordsCoroutine(words));
+        }
+
+
+        private IEnumerator ProcessWordsCoroutine(string[] words)
+        {
+            foreach (string word in words)
+            {
+                if (string.IsNullOrEmpty(word)) continue;
+              
+                if (wordAnimations.ContainsKey(word))
+                {
+                    string animationName = wordAnimations[word];
+                    characterAnimator.CrossFade(animationName,( 0.1f));
+                    Debug.Log($"Playing word animation '{animationName}' for: {word}");
+                    //yield return new WaitForSeconds(AnimationClipLength[word]);
+                    yield return new WaitForSeconds(3f);
+                }
+                else
+                {
+                    Debug.Log($"No word animation found for: {word}. Breaking into letters.");
+                    yield return StartCoroutine(AnimateWordLettersCoroutine(word));
+                }
+            }
+
+            characterAnimator.CrossFade("idle", 0.2f);
+        }
+
+        private IEnumerator AnimateWordLettersCoroutine(string word)
+        {
+            foreach (char c in word)
+            {
+                char upperChar = char.ToUpper(c);
+                string animationName = upperChar.ToString();
+                Debug.Log(upperChar + " :Capital Letter Anim Word");
+                characterAnimator.CrossFade(animationName,0.1f);
+                //yield return new WaitForSeconds(AnimationClipLength[animationName]);
+                yield return new WaitForSeconds(3f);
+            }
+
+            characterAnimator.CrossFade("idle", 0.2f);
         }
 
         private float CalculateSimilarity(string source, string target)
